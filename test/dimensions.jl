de2163721c1cb586da259f08cfc6958b52f3b633
lloyd konneker <konnekerl@gmail.com>
2021-09-20 09:37:19 -0400

    Fix 4D test

--- test/dimensions.jl
+++ test/dimensions.jl
@@ -24,78 +24,79 @@ using FileIO    # For "load" local  files
 using ColorTypes
 
 
-# This is executable, not a definition
-# Each test_reference is a test of the testset
+#=
+This is executable, not a definition
+Each let;test_reference;end is a test of the testset
+Use let;end for separate scope in each test (no spillover of bindings)
+=#
 @testset "Dimensions" begin
-
 
     # 1D text
     # Not working, index is Int64 instead of Cartesian{1}
 
 
-
-
     # 2D text
     # exactly 72 bytes long
-    inPath = "/home/bootch/git/resynthesizerJulia/test/data/in/tiny.txt"
-    # !!! load will not load a text, use read()
-    file = open(inPath)
-    textStream = read(file, 72)
-    # reshape to an array, a pseudo-poem
-    data = reshape(textStream, 6, 12)
+    let
+        inPath = "/home/bootch/git/resynthesizerJulia/test/data/in/tiny.txt"
+        # !!! load will not load a text, use read()
+        file = open(inPath)
+        textStream = read(file, 72)
+        # reshape to an array, a pseudo-poem
+        data = reshape(textStream, 6, 12)
 
-    mask = falses(size(data))
-    mask[3:4, 2:3] .= true
+        mask = falses(size(data))
+        mask[3:4, 2:3] .= true
 
-    outPath = "data/out/tiny.txt"
-    @test_reference outPath resynthesize(data, mask)
-
+        outPath = "data/out/tiny.txt"
+        @test_reference outPath resynthesize(data, mask)
+    end
 
 
     # 2D color image
+    let
+        inPath = "/home/bootch/git/resynthesizerJulia/test/data/in/tinylighthouse.png"
+        data = load(inPath)
+        mask = falses(size(data))
+        mask[3:4, 2:3] .= true
 
-    inPath = "/home/bootch/git/resynthesizerJulia/test/data/in/tinylighthouse.png"
-    data = load(inPath)
-    mask = falses(size(data))
-    mask[3:4, 2:3] .= true
-
-    outPath = "data/out/tinylighthouse.png"
-    @test_reference outPath resynthesize(data, mask)
-
-
-
+        outPath = "data/out/tinylighthouse.png"
+        @test_reference outPath resynthesize(data, mask)
+    end
 
 
     # 3D gray (slices of an mri)
-    using TestImages
-    fullImage = testimage("mri");
-    print(summary(fullImage))
+    let
+        using TestImages
+        fullImage = testimage("mri");
+        print(summary(fullImage))
 
-    # See JuliaImages. The image has named axes. We only want to convert the data.
-    data = fullImage.data
+        # See JuliaImages. The image has named axes. We only want to convert the data.
+        data = fullImage.data
 
-    # mask
-    mask = falses(size(data))
-    # synthesize a small cube in the horizontal slices 5 and 6
-    mask[3:4, 2:3, 5:6] .= true
+        # mask
+        mask = falses(size(data))
+        # synthesize a small cube in the horizontal slices 5 and 6
+        mask[3:4, 2:3, 5:6] .= true
 
-    out = resynthesize(data, mask)
+        out = resynthesize(data, mask)
 
-    outPath = "data/out/mri"
-    #=
-    TODO this fails because test_reference fails to handle 3D???
+        outPath = "data/out/mri"
+        #=
+        TODO this fails because test_reference fails to handle 3D???
 
-    @test_reference outPath resynthesize(data, mask)
+        @test_reference outPath resynthesize(data, mask)
 
-    Consequently, we iteratively compare some slices of the image.
-    =#
+        Consequently, we iteratively compare some slices of the image.
+        =#
 
-    # The first horizontal (superior axis) plane is at z=5
-    slice = out[:,:,5]
-    print(summary(slice))
+        # The first horizontal (superior axis) plane is at z=5
+        slice = out[:,:,5]
+        print(summary(slice))
 
-    # compare one slice to the reference
-    @test_reference outPath slice
+        # compare one slice to the reference
+        @test_reference outPath slice
+    end
 
 
 
@@ -107,47 +108,55 @@ using ColorTypes
     # 4D e.g. time lapsed 3D images, or time lapses 2D images with explicit channels?
     # copied from test/4D/multi.jl
     # "multi" is short name
-    fullImage = testimage("multi");
+    let
+        fullImage = testimage("multi");
 
-    println("fullImage is:")
-    println(summary(fullImage))
-    # 167×439×3×7
+        println("fullImage is:")
+        println(summary(fullImage))
+        # 167×439×3×7
 
-    #=
-    See JuliaImages.
-    The image has named axes.
-    And metadata??
-    We only want to convert the data.
-    Extract: data(fullImage), not fullImage.data??
-    =#
-    imageWithAxes = data(fullImage)
-    imageView = imageWithAxes.data
-    #=
-    imageView is still reshape(reinterpret(...)) which throws type errors.
-    Its a view. So copy it.
-    =#
-    image = copy(imageView)
+        #=
+        See JuliaImages.
+        The image has named axes and metadata??
+        We only want to convert the data.
+        =#
+        using ImageMetadata
 
-    # Create mask
-    mask = falses(size(image))
-    # ??? The third dimension is 3 channels.  Arbitrarily take them all.
-    mask[3:4, 2:3, 1:3, 5:6] .= true
+        # Extract: data(fullImage), not fullImage.data?? data() is a method of ImageMetadata
+        imageWithAxes = data(fullImage)
 
-    # test
-    out = resynthesize(data, mask)
+        imageView = imageWithAxes.data
+        #=
+        imageView is still reshape(reinterpret(...)) which throws type errors.
+        Its a view. So copy it.
+        =#
+        image = copy(imageView)
 
-    outPath = "data/out/multi"
-    #=
-    test_reference fails to handle 3D, 4D ???
-    Consequently, we iteratively compare some slices of the image.
-    =#
-    println("typeof(out) is")
-    println(typeof(out))
+        # Create mask
+        mask = falses(size(image))
+        # ??? The third dimension is 3 channels.  Arbitrarily take them all.
+        mask[3:4, 2:3, 1:3, 5:6] .= true
 
-    # Don't care what this slice is, u=5
-    slice = out[:,:,:,5]
-    print(summary(slice))
+        # test
+        out = resynthesize(image, mask)
 
-    # compare one slice to the reference
-    #@test_reference outPath slice
+        outPath = "data/out/multi"
+        #=
+        test_reference fails to handle 3D, 4D ???
+        Consequently, we iteratively compare some 2D slices of the image.
+        =#
+
+        #=
+        I'm not sure what this slice represents.
+        z=1 is the first of three channels?
+        u=5 is the 5th of 7 time slots?
+        Anyway, it must be 2D for test_reference.
+        And the slice is through the mask, the synth region.
+        =#
+        slice = out[:,:,1,5]
+        print(summary(slice))
+
+        # compare one slice to the reference
+        @test_reference outPath slice
+    end
 end
